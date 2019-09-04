94df58af7c62416b682f085579bd613321235503
Courtney Walker <cwalked@gmail.com>
2019-09-04 13:28:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,11 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  { key => value }
 end
 
 def read_from_hash(hash, key)
-  { key => value }
+ hash[key]
 end
 
 def update_counting_hash(hash, key)
